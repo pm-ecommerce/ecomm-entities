fa4479a4fee3f0e7effbda23dbf81e305f6a283f
Sandeep Giri <ioesandeep@gmail.com>
2020-10-27 16:42:58 -0400

    Entities fix for products

--- src/main/java/com/pm/ecommerce/entities/ProductAttribute.java
+++ src/main/java/com/pm/ecommerce/entities/ProductAttribute.java
@@ -3,14 +3,12 @@ package com.pm.ecommerce.entities;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.util.List;
+import java.util.Set;
 
 @Entity
 @Data
 @Table(name = "product_attributes")
 public class ProductAttribute extends Attribute {
-    @OneToMany(cascade = CascadeType.ALL, targetEntity = Option.class, fetch = FetchType.LAZY)
-    private List<Option> options;
+    @OneToMany(cascade = CascadeType.ALL, targetEntity = Option.class, fetch = FetchType.LAZY, orphanRemoval = true)
+    private Set<Option> options;
 }
-
-
