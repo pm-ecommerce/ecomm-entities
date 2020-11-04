851ebb66fd474af22e2558acc01f6773c9c603fe
Sandeep Giri <ioesandeep@gmail.com>
2020-11-04 18:48:14 -0500

    Encrypt password

--- src/main/java/com/pm/ecommerce/entities/Account.java
+++ src/main/java/com/pm/ecommerce/entities/Account.java
@@ -2,9 +2,13 @@ package com.pm.ecommerce.entities;
 
 import lombok.Data;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.security.spec.KeySpec;
 import java.sql.Timestamp;
+import java.util.Base64;
 
 @Entity
 @Data
@@ -28,14 +32,57 @@ public class Account {
 
     private Timestamp updatedDate;
 
-    // hash the password here
+    private final static byte[] SALT = {
+            (byte) 0xc9,
+            (byte) 0x36,
+            (byte) 0x78,
+            (byte) 0x99,
+            (byte) 0x52,
+            (byte) 0x3e,
+            (byte) 0xea,
+            (byte) 0xf2
+    };
+
     @PrePersist
     protected void prePersist() {
-
+        password = encryptPassword(password);
     }
 
-    // hash the password here
     @PreUpdate
     protected void preUpdate() {
+        password = encryptPassword(password);
+    }
+
+    private String encryptPassword(String password) {
+        try {
+            KeySpec spec = new PBEKeySpec(password.toCharArray(), SALT, 65536, 128);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            return Base64.getEncoder().encodeToString(factory.generateSecret(spec).getEncoded());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
+
+    public boolean verify(String encoded, String pass) throws Exception {
+        String encrypted = encryptPassword(pass);
+
+        if (encrypted == null) {
+            throw new Exception("Password could not be encoded");
+        }
+
+        return encrypted.equals(encoded);
+    }
+
+    public boolean verify(String pass) throws Exception {
+        String encrypted = encryptPassword(pass);
+
+        if (encrypted == null) {
+            throw new Exception("Password could not be encoded");
+        }
+
+        return encrypted.equals(password);
     }
 }
